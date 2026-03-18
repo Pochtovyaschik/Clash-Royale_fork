f391f663ae017af9ea127fea9a2192149b778d16
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-03-18 18:35:01 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // Здесь будут вызовы
+    
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();;
